559664d3321353bc66a169cff165248bae7fa32f
sathninduk <45420836+sathninduk@users.noreply.github.com>
2025-08-31 14:41:16 +0530

    fix: Missing Anti-clickjacking Header

---
+++ src/main/java/config/ContentSecurityPolicyFilter.java
@@ -0,0 +1,24 @@
+package config;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ContentSecurityPolicyFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if (response instanceof HttpServletResponse) {
+            HttpServletResponse httpResponse = (HttpServletResponse) response;
+            httpResponse.setHeader("Content-Security-Policy", "frame-ancestors 'self'");
+        }
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
